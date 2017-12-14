a266e66140e0dbb3023fd46a2a125383e5a1566b
Leigh Kaszick <lkaszick@nowdigital.com.au>
2017-12-14 21:12:20 +1100

    Removed override for createJSModules as this method has been removed from React Native version 0.47.0.

--- android/src/main/java/com/tectiv3/aes/RCTAesPackage.java
+++ android/src/main/java/com/tectiv3/aes/RCTAesPackage.java
@@ -21,11 +21,6 @@ public class RCTAesPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
     }
